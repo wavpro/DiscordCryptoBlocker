ccde86ab2baf0e8cb640028e60cc4273adab389d
wavpro <31097540+wavpro@users.noreply.github.com>
2021-01-01 00:31:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,8 +39,7 @@ client.on('message', async message => {
   if (contentnum > 2) {
     if (message.content.length > 120) {
       await message.delete()
-      await message.author.send('Hey! It seems like you have been banned from the AirReps server. This action was done by our anti bot protection. We are truly sorry if this action was wrong. If you are not a bot, join https://discord.gg/phwbfDPSwz and ask to be unbanned.');
-  await message.member.ban(({
+      await message.member.ban(({
                 reason: 'Crypto Scammer'
   }))
     }
